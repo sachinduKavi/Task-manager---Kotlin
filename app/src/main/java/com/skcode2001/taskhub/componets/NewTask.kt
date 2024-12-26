2e1a2f1432f5c209fe100d9bc7e1fd265adbc3f2
Ishini Dewamiththa <ishinidewamiththa@gmail.com>
2024-12-27 01:17:37 +0530

    created sample box

--- app/src/main/java/com/skcode2001/taskhub/componets/NewTask.kt
+++ app/src/main/java/com/skcode2001/taskhub/componets/NewTask.kt
@@ -11,8 +11,11 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Close
@@ -24,7 +27,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.skcode2001.taskhub.ui.theme.transparentDark
@@ -37,6 +44,7 @@ fun NewTask(padding: PaddingValues, overLayOn: MutableState<Boolean>) {
 
     var name = remember { mutableStateOf("") }
     var description = remember { mutableStateOf("") }
+    var color = remember { mutableStateOf("") }
 
     Box(modifier = Modifier
         .fillMaxSize()
@@ -96,7 +104,22 @@ fun NewTask(padding: PaddingValues, overLayOn: MutableState<Boolean>) {
             ) {
                 CustomTextField("Name", name)
                 CustomTextField("Description", description)
+
+                Column{
+                    Text(
+                        text = "Colour",
+                        color = Color.White
+                    )
+                    Box(modifier = Modifier
+                            .size(60.dp)
+                            .clip(RectangleShape)
+                            .background(Color.Blue)){
+
+                    }
+                }
             }
+
+
 
         }
     }
